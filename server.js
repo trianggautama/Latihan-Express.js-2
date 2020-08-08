9731df6b8f24dd94e5e86e4e41eb71cb29f8c707
tri angga utama <trianggategarutama@gmail.com>
2020-08-09 06:30:27 +0800

    basic middleware

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require("express")
 const app     = express()
 const port    = 3000
 
+//middleware
+const middleware1 = (req, res, next) => {
+    console.log('Middleware berjalan')
+    next()
+}
+
+const middleware2 = (req, res, next) => {
+    console.log('Middleware 2 berjalan')
+    next()
+}
+
+app.use(middleware1)
+app.use(middleware2)
+
+
 app.get('/',(req,res)=>{
     res.send("Hallo world")
 })
