ef6f9d37fffeefb472fa3532f8bc4ed48a7dba0f
tri angga utama <trianggategarutama@gmail.com>
2020-08-09 07:35:35 +0800

    example template view

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require("express")
+const express = require('express')
 const app     = express()
 const port    = 3000
+const path    = require('path')
+
+//setup view engine 
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine', 'hbs')
+
 
 //middleware
 const middleware1 = (req, res, next) => {
@@ -46,7 +52,17 @@ app.use(middleware2)
         res.sendFile('/Users/triangga/pictures/Screen Shot 2020-03-04 at 18.50.26.png')
     })
 
+    //rendering template view
+    app.get('/halaman',(req, res) =>{
+        res.render('index')
+    })
 
+    //rendering temaplate view with data
+    app.get('/with-data/:data',(req,res) =>{
+        res.render('with-data',{
+            data    : req.params.data
+        })
+    })
 //end routing
 
 app.listen(port,()=>{
