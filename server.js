b7271a55c2c397a271fd1e3c27fbd3d7a360b28c
tri angga utama <trianggategarutama@gmail.com>
2020-08-09 07:07:51 +0800

    routing

--- server.js
+++ server.js
@@ -15,11 +15,39 @@ const middleware2 = (req, res, next) => {
 
 app.use(middleware1)
 app.use(middleware2)
+//end middleware
 
 
-app.get('/',(req,res)=>{
-    res.send("Hallo world")
-})
+//routing
+
+    //basic get routing
+    app.get('/',(req,res)=>{
+        res.send("Hallo world")
+    })
+
+    //with param example
+    const message = (req,res) => {
+        res.send(`namanya saya  : ${req.params.nama}`)
+    }
+
+    app.get('/with-params/:nama',message)
+
+    //redirect route 
+    app.get('/redirect',(req, res) => {
+        res.redirect('https://github.com/trianggautama')
+    })
+
+    //redirect local route 
+    app.get('/local-redirect',(req, res) => {
+        res.redirect('/')
+    })
+
+    app.get('/sendFile',(req,res) => {
+        res.sendFile('/Users/triangga/pictures/Screen Shot 2020-03-04 at 18.50.26.png')
+    })
+
+
+//end routing
 
 app.listen(port,()=>{
     console.log(`server berjalan pada port : ${port}`)
