2dffb833b13d3a6831f70f3df8c07b427853ebd8
tri angga utama <trianggategarutama@gmail.com>
2020-08-09 09:13:41 +0800

    latihan project dasar

--- server.js
+++ server.js
@@ -2,35 +2,63 @@ const express = require('express')
 const app     = express()
 const port    = 3000
 const path    = require('path')
+const data    = require('./data/items.json')
+const bodyParser = require('body-parser')
 
 //setup view engine 
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'hbs')
 
+// setup static derictory
+app.use(express.static('public'))
+
+//setup body parser
+app.use(bodyParser.urlencoded({ 
+    extended:false 
+}))
 
 //middleware
-const middleware1 = (req, res, next) => {
-    console.log('Middleware berjalan')
-    next()
-}
+    // const middleware1 = (req, res, next) => {
+    //     console.log('Middleware berjalan')
+    //     next()
+    // }
 
-const middleware2 = (req, res, next) => {
-    console.log('Middleware 2 berjalan')
-    next()
-}
+    // const middleware2 = (req, res, next) => {
+    //     console.log('Middleware 2 berjalan')
+    //     next()
+    // }
 
-app.use(middleware1)
-app.use(middleware2)
+    // app.use(middleware1)
+    // app.use(middleware2)
 //end middleware
 
 
 //routing
 
     //basic get routing
-    app.get('/',(req,res)=>{
-        res.send("Hallo world")
-    })
+    // app.get('/',(req,res)=>{
+    //     res.send("Hallo world")
+    // })
 
+    //rendering template view
+        app.get('/',(req, res) =>{
+            res.render('index',{
+                items : data
+            })
+        })
+
+    // post route example
+        app.get('/form',(req, res) =>{
+            res.render('form')
+        })
+
+        app.post('/form',(req, res) =>{
+
+            res.render('form',{
+                name : req.body.name
+            })
+        })
+    
     //with param example
     const message = (req,res) => {
         res.send(`namanya saya  : ${req.params.nama}`)
@@ -52,10 +80,6 @@ app.use(middleware2)
         res.sendFile('/Users/triangga/pictures/Screen Shot 2020-03-04 at 18.50.26.png')
     })
 
-    //rendering template view
-    app.get('/halaman',(req, res) =>{
-        res.render('index')
-    })
 
     //rendering temaplate view with data
     app.get('/with-data/:data',(req,res) =>{
